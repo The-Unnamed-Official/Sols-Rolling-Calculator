9df3f2549b5a6e610c05fec382cc1b9b31981e28
beanshahalololl-afk <beanshahalololl@gmail.com>
2025-10-18 13:30:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -391,7 +391,7 @@ for (const [eventId, auraNames] of Object.entries(EVENT_AURA_MAP)) {
     });
 }
 
-const cutscenePriority = ["oblivion-cs", "memory-cs", "equinox-cs", "lumi-cs", "erebus-cs", "Lamenthyr-cs" "pixelation-cs", "dreammetric-cs", "oppression-cs", "glitch-cs"];
+const cutscenePriority = ["oblivion-cs", "memory-cs", "equinox-cs", "lumi-cs", "erebus-cs", "Lamenthyr-cs", "pixelation-cs", "dreammetric-cs", "oppression-cs", "glitch-cs"];
 
 oblivionAuraDefinition = auras.find(aura => aura.name === OBLIVION_AURA_NAME) || null;
 memoryAuraDefinition = auras.find(aura => aura.name === OBLIVION_MEMORY_AURA_NAME) || null;
